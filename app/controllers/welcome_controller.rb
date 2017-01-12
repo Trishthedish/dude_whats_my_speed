b0267374c94fdfd68afebc827f2115b0bf943ba5
Trish Griego <pmgriego@gmail.com>
2017-01-11 19:05:05 -0800

    Still attempting to get geocoder to work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
 
 def index
   @search_results = Broadband_Wrapper.search_broadband
+  @locations = Location.all
 
-  if params[:search].present?
-    @location = Location.new
+  if params[:search]
+    @locations = Location.search(params[:search])
   else
-
-
+    @locations = Location.all
   end
 
 end
