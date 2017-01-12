6a63ed51a6801269e2b362322627a053480f0b1b
Trish Griego <pmgriego@gmail.com>
2017-01-11 18:48:06 -0800

    Attempting to move logic that was working in my geocode repo into my dude_whats_my_speed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,16 @@ class WelcomeController < ApplicationController
 def index
   @search_results = Broadband_Wrapper.search_broadband
 
+  if params[:search].present?
+    @location = Location.new
+  else
+
+
+  end
+
 end
 
 end
+
+
+#Geocoder.coordinates("25 East Hempishere.")
