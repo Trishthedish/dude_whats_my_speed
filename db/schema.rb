90365fa2cc24cb22d1256c254c786cdc5de2a362
Trish Griego <pmgriego@gmail.com>
2017-01-23 21:29:45 -0800

    added paid_ping attribute to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124021411) do
+ActiveRecord::Schema.define(version: 20170124052725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170124021411) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "service_provider"
+    t.decimal  "paid_ping"
   end
 
 end
