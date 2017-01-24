3f54ce7725bd3ab6079d42883101cfe8f24c7eea
Trish Griego <pmgriego@gmail.com>
2017-01-23 21:24:14 -0800

    added preliminary data for seed user and rasberrypi. realized model for user needs to have paid_ping attribute.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'CSV'
+#
+# CSV.foreach('seed_csvs/users_cap.csv') do |csv_obj|
+#   User.create(id: csv_obj[0].to_i, name: csv_obj[1], monthly_cost: csv_obj[2], paid_download: csv_obj[3], paid_upload: csv_obj[4], area_code: csv_obj[5], service_provider: csv_obj[6])
+# end
+#
+# # need to add paid ping field to users model. Then add it to csv file and updaate the importing of it.
+# CSV.foreach('seed_csvs/rasberrypi_cap.csv') do |csv_obj|
+#   RasberryPi.create(id:csv_obj[0].to_i, ping: csv_obj[1], download: csv_obj[2], upload: csv_obj[3], user_id: csv_obj[4])
+# end
