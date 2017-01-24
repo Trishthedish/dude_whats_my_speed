e376d4596aff7c1f20cd3cf955fb930af1be7cfb
Trish Griego <pmgriego@gmail.com>
2017-01-23 18:01:27 -0800

    Made decsion to keep users model as previously described and planned. Added service_provide name back into database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124015353) do
+ActiveRecord::Schema.define(version: 20170124020001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,9 @@ ActiveRecord::Schema.define(version: 20170124015353) do
     t.decimal  "paid_upload"
     t.integer  "area_code"
     t.integer  "api_key"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "service_provider"
   end
 
 end
