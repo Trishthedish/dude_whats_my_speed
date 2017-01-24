c071477a71fc8ebca6b60670ee32b2eb3181fe61
Trish Griego <pmgriego@gmail.com>
2017-01-23 22:03:02 -0800

    Created a properly formated users model that is now rendering to page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,33 @@ class UsersController < ApplicationController
 
   end
 
-  def show
-    @my_user = RasberryPi.find(params[:id])
-    @my_pi = @my_user.rasberry_pis
+  def create
+    @myuser = User.new
+    @myuser.name = params[:user][:name]
+    @myuser.monthly_cost = params[:user][:name]
+    @myuser.paid_download = params[:user][:name]
+    @myuser.paid_upload = params[:user][:name]
+    @myuser.area_code = params[:user][:name]
+    @myuser.service_provider = params[:user][:name]
+    if @myuser.save
+      logger.debug "user created!"
+      render json: {status: "SUCCESS", message: "loaded all user data."}, status: :ok
+    end
 
-    if @my_user == nil
+  end
+
+  def show
+    @myuser = User.find(params[:id])
+    @mypi = @myuser.rasberry_pis
+
+    if @myuser == nil
       render :file => 'public/404.html', :status => :not_found
     end
   end
 
+def edit
+
+end
 
 
 end
