3293cbed6125c2709a896ab2e8129a74bf315b04
Trish Griego <pmgriego@gmail.com>
2017-01-24 12:50:45 -0800

    Seeded the database with new dates and times. Formated it like: %Y-%m-%d %H:%M:%s.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # require 'CSV'
+
+CSV.foreach('seed_csvs/users_cap.csv') do |csv_obj|
+  User.create(id: csv_obj[0].to_i, name: csv_obj[1], monthly_cost: csv_obj[2], paid_download: csv_obj[3], paid_upload: csv_obj[4], area_code: csv_obj[5], service_provider: csv_obj[6])
+end
 #
-# CSV.foreach('seed_csvs/users_cap.csv') do |csv_obj|
-#   User.create(id: csv_obj[0].to_i, name: csv_obj[1], monthly_cost: csv_obj[2], paid_download: csv_obj[3], paid_upload: csv_obj[4], area_code: csv_obj[5], service_provider: csv_obj[6])
-# end
-#
-# CSV.foreach('seed_csvs/rasberrypi_cap.csv') do |csv_obj|
-#   RasberryPi.create(id:csv_obj[0].to_i, ping: csv_obj[1], download: csv_obj[2], upload: csv_obj[3], user_id: csv_obj[4])
-# end
+CSV.foreach('seed_csvs/rasberrypi_cap.csv') do |csv_obj|
+  RasberryPi.create(id:csv_obj[0].to_i, ping: csv_obj[1], download: csv_obj[2], upload: csv_obj[3], user_id: csv_obj[4], date_time: csv_obj[5])
+end
